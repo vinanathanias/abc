bc4d9ad03e40507c644368745106c0cb75330548
vinanathanias <90330804+vinanathanias@users.noreply.github.com>
2025-02-26 21:15:59 +0700

    Update ui.py

--- ui.py
+++ ui.py
@@ -27,8 +27,30 @@ st.set_page_config(page_title="Clustering Apps",
 
 ### LAYOUT ###
 st.header("Welcome to Clustering Apps", anchor=False)
+
 with st.container(border=True):
-    st.write("This is inside the container")
+    st.write("""
+    ### Welcome to our Customer Segmentation Tool!  
+    
+    Harness the power of **K-Means clustering** to gain valuable insights into your online retail dataset. 
+    Our platform allows you to effortlessly segment customers, helping you understand their behavior and tailor your marketing strategies.  
+    
+    **Key Features:**  
+    - **Flexible Clustering:** Adjust the number of clusters to suit your needs, or leverage the recommended optimum clusters using Silhouette Score and Elbow Score for the best segmentation results.  
+    - **Data Visualization:** Easily identify customer patterns and behaviors through intuitive visualizations, empowering you to make informed business decisions.  
+    - **Simple Data Upload:** Our tool accepts datasets with the following columns:  
+      - InvoiceNo  
+      - StockCode  
+      - Description  
+      - Quantity  
+      - InvoiceDate  
+      - UnitPrice  
+      - CustomerID  
+      - Country  
+    
+    Start exploring your customer data today and uncover meaningful insights to grow your business!
+    """)
+
 
 # Path to the ZIP file containing the existing data
 existing_data_zip_path = "data/filtered_data.zip"

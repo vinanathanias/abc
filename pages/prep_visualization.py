7d7f51d1b96df89e4b6422a0c605f4866d64b686
vinanathanias <90330804+vinanathanias@users.noreply.github.com>
2025-03-13 21:19:45 +0700

    Update prep_visualization.py

--- pages/prep_visualization.py
+++ pages/prep_visualization.py
@@ -160,11 +160,11 @@ def calculate_average_scores_per_cluster(data):
 # Main function to display preprocessing and visualization
 def main():
     st.subheader("Preprocessing and Visualization")
-    st.write(f'''Data preparation for RFM (Recency, Frequency, Monetary) analysis involves cleaning and 
+    st.write(f'''**Data preparation for RFM (Recency, Frequency, Monetary) analysis involves cleaning and 
     transforming transaction data to compute RFM values per customer and per month. 
     This process includes handling outliers, normalizing the data, aggregating transactions by customer-month, 
     and calculating Recency (days since last purchase), Frequency (number of transactions), and Monetary (total spend). 
-    The prepared dataset enables customer segmentation and trend analysis.''')
+    The prepared dataset enables customer segmentation and trend analysis.**''')
 
     # Retrieve the dataframe from session state
     if 'df' in st.session_state:
@@ -174,12 +174,12 @@ def main():
         return
 
     # Display the dataframe
-    st.write("This is the raw data to be processed for calculating RFM values.")
+    st.write("This is the data to be processed for calculating RFM values:")
     st.dataframe(df, use_container_width=True)
 
     # Calculate RFM data
     monthly_data = calculate_rfm(df)
-    st.write("Below is the processed RFM data, aggregated per customer per month.")
+    st.write("Below is the processed RFM data, aggregated per customer per month: ")
     st.dataframe(monthly_data, use_container_width=True)
 
     # Visualize data BEFORE handling outliers
